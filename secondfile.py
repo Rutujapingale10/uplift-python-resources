91af8747ee56b839385dda8a1e2dedde78ef3270
Sagnik Mitra <sagnikmitra123@gmail.com>
2021-06-27 20:10:11 +0530

    Second File Demo Upload

---
+++ secondfile.py
@@ -0,0 +1,16 @@
+# Functions are generators
+# we put some input in python and wwe get some output
+"""
+x = 2, print((2*1)x)
+x = 3, print((3*2)x)
+"""
+# Fucntion definition has a partiucar syntax and we have to follow that
+
+
+def my_function(x, y):
+    # we declared a fucntion named my_fcuntion and the fucntuion has some parameters
+    result = x*(y-1)*x
+    print(result)
+
+
+my_function(5, 6)
