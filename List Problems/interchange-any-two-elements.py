afe77d4d4663c699f2386455d4b31da5fb629ffc
sagnikmitra <sagnikmitra123@gmail.com>
2021-07-01 20:01:22 +0530

    2nd problem any two elemnts

--- List Problems/interchange-any-two-elements.py
+++ List Problems/interchange-any-two-elements.py
@@ -1,10 +1,3 @@
-# uplift-python-resources
-Sagnik Mitra | Mentor
-
-## List Problems
-1. Python Program to Interchange First and Last element of a List
-* Solution:\
-```python
 """
 [12,35,9,56,24]
 [24,35,9,56,12]
@@ -15,50 +8,6 @@ No, how to swap two elements.
 For that the best and easiest solution (not the most efficient) is to take another variable as a temp variable
 """
 
-
-def intFirstLast(myList):
-    sizeOfList = len(myList)
-    # Swapping of the elements
-    tempVar = myList[0]
-    myList[0] = myList[sizeOfList - 1]
-    myList[sizeOfList - 1] = tempVar
-    return myList
-
-
-def intFirstLastSecondApproach(myList):
-    # Swapping of the elements
-    tempVar = myList[0]
-    myList[0] = myList[-1]
-    myList[-1] = tempVar
-    return myList
-
-
-def intFirstLastXYApproach(myList):
-    myList[0], myList[-1] = myList[-1], myList[0]
-    return myList
-
-
-def intFirstLastStarOperand(myList):
-    a, *b, c = myList
-    myList = [c, *b, a]
-    return myList
-
-
-# Main Part of the Code / Driver Section of the Code
-newList = [15, 15, 12, 26, 4, 8, 9, 396]
-print(intFirstLast(newList))
-newList = [15, 15, 12, 26, 4, 8, 9, 396]
-print(intFirstLastSecondApproach(newList))
-newList = [15, 15, 12, 26, 4, 8, 9, 396]
-print(f"Using the , switch method: {intFirstLastXYApproach(newList)}")
-newList = [15, 15, 12, 26, 4, 8, 9, 396]
-print(f"Using the * operand approach: {intFirstLastStarOperand(newList)}")
-
-```
-
-2. Python Program to Interchange any two elements of a List
-* Solution:\
-```python
 
 def swapEl(myList, pos1, pos2):
     sizeOfList = len(myList)
@@ -97,6 +46,3 @@ newList = [15, 15, 12, 26, 4, 8, 9, 396]
 print(f"Using the , switch method: {swapElXYApproach(newList,1,5)}")
 newList = [15, 15, 12, 26, 4, 8, 9, 396]
 print(f"Using the * operand approach: {swapElStarOperand(newList,1,2)}")
-
-
-```
