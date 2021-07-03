a7c37443fc21f4ec73635bcad868c78bea16748f
sagnikmitra <sagnikmitra123@gmail.com>
2021-07-03 20:24:47 +0530

    misc program description

---
+++ Tuple Problems/misc.py
@@ -0,0 +1,13 @@
+"""
+Lets' say we have four tuples and we have four list
+Now, we need to write a program where we can have all those elements but in an alterate manner
+but the twist is the user would choose the first set of elements
+
+[1,2] [3,4] [5,6] [7, 8, 9]
+(11,12) (13,14) (15,16) (17, 18, 19)
+
+1 for list and 2 for tuple 
+so, if the user says that i want 1 
+then the entry will be 1 2 11 12 3 4 13 14 5 6 15 16 7 8 9 17 18 19
+then the entry will be 1 2 11 12 3 4 13 14 5 6 15 16 7 8 9 17 18 19
+"""
