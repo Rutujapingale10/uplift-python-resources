fd0fbfb9099ba1d22b5db9a93f546d2f262c3056
Meenu Singh <meenubgp005@gmail.com>
2021-07-08 16:06:41 +0530

    RaceCar problem solution

--- RaceCar Problem Submission/racecar_meenu.py
+++ RaceCar Problem Submission/racecar_meenu.py
@@ -2,7 +2,7 @@ car_list = input("Enter the list : ")
 car_list =  car_list.split()
 def overtake():
     overtaken = 0
-    overtaken = [(overtaken+1) for i in range(len(car_list)) for j in range(i,len(car_list)) if (car_list[i]>car_list[j])]
+    overtaken = [(overtaken+1) for i in range(len(car_list)) for j in range(i+1,len(car_list)) if (car_list[i]>car_list[j])]
     return len(overtaken)
 
 print("Total nnumber of overtake is : ", overtake())
