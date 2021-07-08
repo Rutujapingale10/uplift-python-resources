e92dd5616a4df6c2762f53f9b00c4046481cbfc4
aditya-upadhye <adityaupadhye47@gmail.com>
2021-07-08 17:50:07 +0530

    Racecar problem solved

--- RaceCar Problem Submission/racecar_aditya.py
+++ RaceCar Problem Submission/racecar_aditya.py
@@ -1,1 +1,10 @@
-print("Demo Solution 1")
+#Defining the given list
+carList = [4, 1, 2, 5]
+overtake = 0
+
+for i in range(len(carList)):
+    for j in range(0, len(carList) - 1):
+        if carList[j] > carList[j + 1]:
+            carList[j], carList[j + 1] = carList[j + 1], carList[j]
+            overtake += 1
+print(f"Number of overtakes are : {overtake}")
