dcaaad65be93b4245c5c3921e9b13ae6c48b9471
shreya-madhwal <shreyamadhwal3@gmail.com>
2021-07-09 10:55:38 -0600

    Modified racecar code by shreya

--- RaceCar Problem Submission/racecar_shreya.py
+++ RaceCar Problem Submission/racecar_shreya.py
@@ -4,9 +4,7 @@ overtake = 0
 for i in range(len(carList)):
     for j in range(0,len(carList)-1):
         if carList[j]>carList[j+1]:
-            temp=carList[j]
-            carList[j]=carList[j+1]
-            carList[j+1]=temp
+            carList[j],carList[j+1]=carList[j+1],carList[j]
             overtake+=1
 print("Number of overtakes are:"+str(overtake))
     
