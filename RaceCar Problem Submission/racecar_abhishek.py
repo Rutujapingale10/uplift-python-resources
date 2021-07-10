f74b1a0ce2460b759dec7c4ae85493a04ff5f172
abhishek21 <Abhishekbhande21@gmail.com>
2021-07-10 14:36:23 +0530

    Abhishek's solution

--- RaceCar Problem Submission/racecar_abhishek.py
+++ RaceCar Problem Submission/racecar_abhishek.py
@@ -25,3 +25,10 @@ Consider the above condition and write a Python Program to calcaulate the total 
 """
 carList = [4, 1, 2, 5]
 overtake = 0
+
+for i in range(len(carList)):
+    for j in range(i+1, len(carList)):
+        if(carList[j] < carList[i]):
+            overtake += 1
+
+print(overtake)
